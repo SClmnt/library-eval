fb29d0e8d6e98eb5f262ff48e9899824f89f1d46
sclmnt <silone.clement@gmail.com>
2024-10-22 14:23:39 +0200

    upload final

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,10 +3,12 @@ import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './book.entity';
+import { Loan } from 'src/loans/loan.entity';
+import { LoansService } from 'src/loans/loans.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book, Loan])],
   controllers: [BooksController],
-  providers: [BooksService]
+  providers: [BooksService, LoansService]
 })
 export class BooksModule {}
--- src/loans/loans.module.ts
+++ src/loans/loans.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LoansController } from './loans.controller';
 import { LoansService } from './loans.service';
+import { Loan } from './loan.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Loan])],
   controllers: [LoansController],
   providers: [LoansService]
 })
